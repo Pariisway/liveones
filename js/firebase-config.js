4b205553906c09493a2e801ad488e3292e1a4f0a
Pariisway <quickhands.us@gmail.com>
2025-11-27 21:34:07 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU2tbinWSOw-N8ce6zMQ9AMKXt-5fj23g",
+  authDomain: "house-of-whispers.firebaseapp.com",
+  projectId: "house-of-whispers",
+  storageBucket: "house-of-whispers.firebasestorage.app",
+  messagingSenderId: "1063333130646",
+  appId: "1:1063333130646:web:9f0d6ddc2927692aaaadb7",
+  measurementId: "G-06QR5LFKJ9"
+};

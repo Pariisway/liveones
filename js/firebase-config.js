3d9e30d4520b836356de3079f4148930449077d4
Pariisway <quickhands.us@gmail.com>
2025-11-28 01:33:01 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// js/firebase-config.js - ADD THIS FILE TO YOUR LIVE SERVER
+const firebaseConfig = {
+  apiKey: "AIzaSyBU2tbinWSOw-N8ce6zMQ9AMKXt-5fj23g",
+  authDomain: "house-of-whispers.firebaseapp.com",
+  projectId: "house-of-whispers",
+  storageBucket: "house-of-whispers.firebasestorage.app",
+  messagingSenderId: "1063333130646",
+  appId: "1:1063333130646:web:9f0d6ddc2927692aaaadb7",
+  measurementId: "G-06QR5LFKJ9"
+};
